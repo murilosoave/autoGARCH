80ab817acff7835369fe714833c1fc8c25f5ba7f
murilosoave <murilo.soave@mediahero.ai>
2025-03-05 20:35:51 -0300

    feat: Add attributes to variance estimator interface

--- autogarch/estimators/variance/variance_estimator_interface.py
+++ autogarch/estimators/variance/variance_estimator_interface.py
@@ -4,6 +4,10 @@ from numpy.typing import ArrayLike
 
 
 class VarianceEstimatorInterface(ABC):
+    type_: str = "variance_estimator"
+    input_assumptions: list = []
+    output_assumptions: list = []
+
     @abstractmethod
     def fit(self, X: ArrayLike, *args, **kwargs):
         raise NotImplementedError
