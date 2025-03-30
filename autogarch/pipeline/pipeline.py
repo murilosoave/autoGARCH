9e38d392cd66c65879f7fa31de105677f99aae8a
murilosoave <murilo.soave@mediahero.ai>
2025-03-30 15:02:45 -0300

    refact: Simplify estimators parametrization

--- autogarch/pipeline/pipeline.py
+++ autogarch/pipeline/pipeline.py
@@ -9,8 +9,8 @@ from autogarch.metrics.metric import Metric
 
 class Pipeline:
     def __init__(self):
-        self.mean_estimators: list = []
-        self.variance_estimators: list = []
+        self.mean_estimators: dict = {}
+        self.variance_estimators: dict = {}
         self.mean_estimator: MeanEstimatorInterface = None
         self.variance_estimator: VarianceEstimatorInterface = None
         self.metrics: list = []
@@ -22,9 +22,9 @@ class Pipeline:
         **kwargs
     ) -> "Pipeline":
         if object.type_ == "mean_estimator":
-            self.mean_estimators.append({object: kwargs})
+            self.mean_estimators[object] = kwargs
         elif object.type_ == "variance_estimator":
-            self.variance_estimators.append({object: kwargs})
+            self.variance_estimators[object] = kwargs
         elif object.type_ == "metric":
             self.metrics.append(object(**kwargs))
         else:
@@ -38,29 +38,27 @@ class Pipeline:
         
         X_array = np.asarray(X, dtype=float)
         results = {"mean": {}, "variance": {}}
-        for mean_estimator in self.mean_estimators:
-            for mean_estimator_class, mean_estimator_params in mean_estimator.items():
-                for variance_estimator in self.variance_estimators:
-                    for variance_estimator_class, variance_estimator_params in variance_estimator.items():
-                        mean_estimator_class.set_params(**mean_estimator_params)
-                        variance_estimator_class.set_params(**variance_estimator_params)
-                        mean_estimator_class.fit(X_array)
-                        variance_estimator_class.fit(mean_estimator_class.residuals)
+        for mean_estimator_class, mean_estimator_params in self.mean_estimators.items():
+            for variance_estimator_class, variance_estimator_params in self.variance_estimators.items():
+                mean_estimator_class.set_params(**mean_estimator_params)
+                variance_estimator_class.set_params(**variance_estimator_params)
+                mean_estimator_class.fit(X_array)
+                variance_estimator_class.fit(mean_estimator_class.residuals)
 
-                        results["mean"][str(mean_estimator_params)] = {
-                            metric_class.__class__.__name__: metric_class.calculate(mean_estimator_class.residuals)
-                            for metric_class in self.metrics
-                        }
-                        results["variance"][str(variance_estimator_params)] = {
-                            metric_class.__class__.__name__: metric_class.calculate(variance_estimator_class.residuals)
-                            for metric_class in self.metrics
-                        }
+                results["mean"][str(mean_estimator_params)] = {
+                    metric_class.__class__.__name__: metric_class.calculate(mean_estimator_class.residuals)
+                    for metric_class in self.metrics
+                }
+                results["variance"][str(variance_estimator_params)] = {
+                    metric_class.__class__.__name__: metric_class.calculate(variance_estimator_class.residuals)
+                    for metric_class in self.metrics
+                }
 
-                        self.best_mean_estimator = min(results["mean"], key=lambda x: results["mean"][x])
-                        self.best_variance_estimator = min(results["variance"], key=lambda x: results["variance"][x])
+                self.best_mean_estimator = min(results["mean"], key=lambda x: results["mean"][x])
+                self.best_variance_estimator = min(results["variance"], key=lambda x: results["variance"][x])
 
-                        self.mean_estimator = mean_estimator_class.set_params(**mean_estimator_params)
-                        self.variance_estimator = variance_estimator_class.set_params(**variance_estimator_params)
+                self.mean_estimator = mean_estimator_class.set_params(**mean_estimator_params)
+                self.variance_estimator = variance_estimator_class.set_params(**variance_estimator_params)
 
         self.fitted = True
 
